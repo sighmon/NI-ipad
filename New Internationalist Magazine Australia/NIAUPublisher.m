e7a6ef2a9de44efc9c063c45e7b8d061eba440b8
pix <pix@pixs-Mac.local>
2013-07-10 16:51:30 +0930

    now gets local info from local.h

--- New Internationalist Magazine Australia/NIAUPublisher.m
+++ New Internationalist Magazine Australia/NIAUPublisher.m
@@ -50,7 +50,7 @@ static NIAUPublisher *instance =nil;
     NSLog(@"getIssuesList");
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0),
                    ^{
-                       NSData* data = [NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://digital.newint.com.au/issues.json"]];
+                       NSData* data = [NSData dataWithContentsOfURL:[NSURL URLWithString:(SITE_URL @"issues.json")]];
                        NSError* error;
                        NSArray *tmpIssues = [NSJSONSerialization
                                              JSONObjectWithData:data //1
