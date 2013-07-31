8eff42d786c9cee9b2985e525c810936a706eb84
pix <pix@pixs-Mac.local>
2013-07-31 16:18:49 +0930

    half-completed article caching

--- New Internationalist Magazine Australia/NIAUArticle.h
+++ New Internationalist Magazine Australia/NIAUArticle.h
@@ -14,19 +14,21 @@ extern NSString *ArticleDidUpdateNotification;
 extern NSString *ArticleFailedUpdateNotification;
 
 @interface NIAUArticle : NSObject {
-    NSString *body;
     NSDictionary *dictionary;
 }
+
+@property(nonatomic, strong, readonly) NSString *body;
 
 @property(nonatomic, strong) NIAUIssue *issue;
 
 -(NSString *)title;
 -(NSString *)teaser;
 -(NSString *)author;
--(NSString *)body;
 
 +(NIAUArticle *)articleWithIssue:(NIAUIssue *)issue andDictionary:(NSDictionary *)dict;
++(NIAUArticle *)articleFromCacheWithIssue:(NIAUIssue *)issue andId:(NSNumber *)index;
 
 -(void)requestBody;
 
 @end
+
