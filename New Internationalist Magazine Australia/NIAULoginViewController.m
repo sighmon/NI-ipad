141383de2f36263f83b8505e78d8adb55679cf2a
pix <pix@pixs-Mac.local>
2013-10-08 18:33:09 +1030

    simple test code using SSKeyChain

--- New Internationalist Magazine Australia/NIAULoginViewController.m
+++ New Internationalist Magazine Australia/NIAULoginViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "NIAULoginViewController.h"
+#import <SSKeychain.h>
 
 @interface NIAULoginViewController ()
 
@@ -29,6 +30,19 @@
 	// Do any additional setup after loading the view.
     
     NSLog(@"TODO: do rails login here.");
+ 
+    NSArray *accounts = [SSKeychain accountsForService:@"NIWebApp"];
+    if([accounts count]>0) {
+        NSDictionary *dict = accounts[0];
+        self.username.text = dict[@"acct"];
+        self.password.text = [SSKeychain passwordForService:dict[@"svce"] account:dict[@"acct"]];
+    } else {
+        self.username.text = @"coolname";
+        self.password.text = @"topseekret";
+    }
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -40,6 +54,22 @@
 - (IBAction)loginButtonTapped:(id)sender
 {
     NSLog(@"TODO: Do stuff.");
+    
+    // delete all entries from keychain (probably a bad idea, we are testing)
+    [[SSKeychain accountsForService:@"NIWebApp"] enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        [SSKeychain deletePasswordForService:obj[@"svce"] account:obj[@"acct"]];
+    }];
+    
+    [SSKeychain setPassword:self.password.text forService:@"NIWebApp" account:self.username.text];
+    
+    if([self.password.text isEqual:@"topseekret"]) {
+        UIAlertView *theAlert = [[UIAlertView alloc] initWithTitle:@"Warning"
+                                                           message:[NSString   stringWithFormat:@"%@ is a dumb password", self.password.text]
+                                                          delegate:self
+                                                 cancelButtonTitle:@"I'm sorry"
+                                                 otherButtonTitles:nil];
+        [theAlert show];
+    }
 }
 
 @end
