0d4a0311f5b67c601cb3bbe27b93182c6073fcea
sighmon <simon.loffler@gmail.com>
2014-02-26 18:05:25 -0500

    Trying a grey gradient background.

--- New Internationalist Magazine Australia/NIAUHelper.m
+++ New Internationalist Magazine Australia/NIAUHelper.m
@@ -21,8 +21,15 @@
     
     CAGradientLayer *gradient = [CAGradientLayer layer];
     gradient.frame = CGRectMake(view.frame.origin.x, view.frame.origin.y, view.frame.size.width*2, view.frame.size.height);
-    UIColor *startColour = [UIColor colorWithHue:0.2111 saturation:0.87 brightness:0.61 alpha:1.0];
-    UIColor *endColour = [UIColor colorWithHue:0.2111 saturation:0.90 brightness:0.25 alpha:1.0];
+    
+    // NI Greens
+//    UIColor *startColour = [UIColor colorWithHue:0.2111 saturation:0.87 brightness:0.61 alpha:1.0];
+//    UIColor *endColour = [UIColor colorWithHue:0.2111 saturation:0.90 brightness:0.25 alpha:1.0];
+    
+    // Greys
+    UIColor *startColour = [UIColor colorWithRed:110/255. green:119/255. blue:128/255. alpha:1.0];
+    UIColor *endColour = [UIColor colorWithRed:61/255. green:66/255. blue:72/255. alpha:1.0];
+    
     gradient.colors = @[(id)[startColour CGColor], (id)[endColour CGColor]];
     gradient.name = @"backgroundGradient";
     [view.layer insertSublayer:gradient atIndex:0];
